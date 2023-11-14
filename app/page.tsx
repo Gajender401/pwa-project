95c77ad4d2f778d01f915ef81486fadfcb00f45d
Gajender <kgajender86@gmail.com>
2023-11-14 20:51:34 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,202 @@
-import Image from 'next/image'
+'use client'
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import ChatMessage from '../components/ChatMessage';
+import { FaArrowLeft } from "react-icons/fa6";
+import { FiEdit } from "react-icons/fi";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { FiPaperclip } from "react-icons/fi";
+import { VscSend } from "react-icons/vsc";
+import { GoPeople } from "react-icons/go";
+import { IoCallOutline } from "react-icons/io5";
+import { BiMessageX } from "react-icons/bi";
+import { MdOutlineCameraAlt } from "react-icons/md";
+import { FiVideo } from "react-icons/fi";
+import { CgFileDocument } from "react-icons/cg";
 
-export default function Home() {
+interface Chat {
+  id: string;
+  message: string;
+  sender: {
+    image: string;
+    is_kyc_verified: boolean;
+    self: boolean;
+    user_id: string;
+  };
+  time: string;
+}
+
+
+const groupChatsByDate = (chats: Chat[]) => {
+  const groupedChats: { [date: string]: Chat[] } = {};
+
+  chats.forEach((chat) => {
+    const date = chat.time.split(' ')[0]; // Extract the date part
+
+    if (!groupedChats[date]) {
+      groupedChats[date] = [];
+    }
+
+    // Sort chats by time before pushing
+    groupedChats[date].push(chat);
+    groupedChats[date].sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime());
+  });
+
+  return groupedChats;
+};
+
+const ChatPage: React.FC = () => {
+  const [chats, setChats] = useState<Chat[]>([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [isTabOpen, setIsTabOpen] = useState(false);
+  const tabRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://qa.corider.in/assignment/chat?page=0');
+        const newData: Chat[] = response.data.chats;
+
+        setChats(newData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs once on mount
+
+  const groupedChats = groupChatsByDate(chats);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
+  };
+
+  const handleTabToggle = () => {
+    setIsTabOpen((prevIsTabOpen) => !prevIsTabOpen);
+  };
+
+  const handleMenuClose = () => {
+    setIsMenuOpen(false);
+  };
+
+  const handleTabClose = () => {
+    setIsTabOpen(false);
+  };
+
+
+  const handleDocumentClick = (event: MouseEvent) => {
+    // Close the menu if it is open and the click is not within the menu
+    if (isMenuOpen && menuRef.current && !(menuRef.current.contains(event.target as Node))) {
+      handleMenuClose();
+    }
+
+    // Close the tab if it is open and the click is not within the tab
+    if (isTabOpen && tabRef.current && !(tabRef.current.contains(event.target as Node))) {
+      handleTabClose();
+    }
+  };
+
+  useEffect(() => {
+    // Attach a click event listener to the document to handle clicks anywhere on the screen
+    document.addEventListener('click', handleDocumentClick);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, [isMenuOpen, isTabOpen]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container mx-auto p-4" >
+
+      <div className='fixed w-full border-b p-4 top-0 left-0 z-40 bg-primaryColor ' >
+        <div className='flex items-center justify-between' >
+          <div className='flex items-center gap-2 font-semibold text-xl' >
+            <FaArrowLeft />
+            Trip 1
+          </div>
+          <FiEdit />
+        </div>
+
+        <div className='flex items-center pt-3 justify-between' >
+          <div className='flex gap-4 items-center ' >
+            <img className='rounded-full h-14' src="/trip.jpeg" alt="trip" />
+            <div>
+              <p className='text-[#606060] font-normal' >
+                From <span className='text-[#141E0D] text-lg font-semibold ' >IGI Airport, T3</span>
+              </p>
+              <p className='text-[#606060] font-normal' >
+                To <span className='text-[#141E0D] text-lg font-semibold ' >Sector 28</span>
+              </p>
+            </div>
+          </div>
+
+          <div className='relative'>
+            <BsThreeDotsVertical size={25} onClick={handleMenuToggle} />
+            {isMenuOpen && (
+              <div ref={menuRef} className='absolute bg-white border rounded-lg shadow-xl z-50 top-8 right-0 transition-all duration-300 ease-in-out '>
+                <div className='flex flex-row items-center border-b p-3 gap-2'>
+                  <GoPeople size={18} />
+                  <p className='text-xs'>Members</p>
+                </div>
+                <div className='flex flex-row items-center border-b px-3 py-2 gap-2'>
+                  <IoCallOutline size={18} />
+                  <p className='text-xs whitespace-nowrap'>Share Number</p>
+                </div>
+                <div className='flex flex-row items-center px-3 py-2 gap-2'>
+                  <BiMessageX size={18} />
+                  <p className='text-xs'>Report</p>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
+      <div className='h-32' />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+      {Object.entries(groupedChats).map(([date, chatsOnDate]) => (
+        <div key={date} className="mb-4 ">
+          <div className='relative h-10' >
+            <div className="relative z-10 flex items-center justify-center  ">
+              <p className='w-fit text-[#B7B7B7] text-xs bg-primaryColor px-2 ' >
+                {date}
+              </p>
+            </div>
+            <div className="absolute w-full h-[1px] bg-[#B7B7B7] top-[7px]"></div>
+          </div>
+          {chatsOnDate.map((chat) => (
+            <ChatMessage key={chat.id} {...chat} />
+          ))}
+        </div>
+      ))}
+
+      <div className='h-8' />
+
+      <div className='fixed w-full bottom-0 bg-[#FAF9F4] left-0 px-4 py-2' >
+        <div className='bg-white px-3 py-2 rounded-lg  flex items-center justify-between' >
+          <input className='text-[#B7B7B7] text-xs focus:outline-none ' type="text" placeholder='Reply to @Rohit Yadav' />
+          <div className='flex flex-row gap-4' >
+            <div className='relative' >
+              {isTabOpen &&
+                <div ref={tabRef} className='absolute bottom-9 -left-12 rounded-full bg-[#008000] flex flex-row gap-3 px-4 py-3 text-white transition-all duration-300 ease-in-out ' >
+                  <MdOutlineCameraAlt size={19} />
+                  <FiVideo size={19} />
+                  <CgFileDocument size={19} />
+                  <img className='absolute -bottom-1.5 left-[54px]  ' src="/Corner.svg" alt="corner" />
+                </div>
+              }
+
+              <FiPaperclip onClick={handleTabToggle} size={20} />
+            </div>
+            <VscSend size={20} />
+          </div>
+        </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default ChatPage;
---
+++ components/ChatMessage.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface ChatMessageProps {
+  message: string;
+  sender: {
+    image: string;
+    is_kyc_verified: boolean;
+    self: boolean;
+    user_id: string;
+  };
+  time: string;
+}
+
+const ChatMessage: React.FC<ChatMessageProps> = ({ message, sender }) => {
+  const isSelfMessage = sender.self;
+
+  const renderMessage = () => {
+    // Safely render HTML using dangerouslySetInnerHTML
+    return { __html: message };
+  };
+
+  return (
+    <div className={`flex text-xs ${isSelfMessage ? 'justify-end' : 'justify-start mr-5'} items-start  space-x-2 mb-4`}>
+      {!isSelfMessage && <img className="rounded-full h-8 w-8" src={sender.image} alt={sender.user_id} />}
+      <div
+        className={`p-2 shadow-md ${
+          isSelfMessage
+            ? 'rounded-lg rounded-br-none bg-[#1C63D5] text-white ml-12 '
+            : 'rounded-lg rounded-tl-none text-[#606060] bg-white'
+        }`}
+        dangerouslySetInnerHTML={renderMessage()}
+      ></div>
+    </div>
+  );
+};
+
+export default ChatMessage;
